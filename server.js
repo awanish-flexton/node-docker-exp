da0f397e1540f348e2ed0069384615674a2c4ee4
Awanish-flexton <awanish@flextoninc.com>
2018-04-10 15:26:16 +0530

    Change in dev api

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
-app.get('/master', (req, res) => {
-  res.send('Hello master\n');
-});
-
-app.get('/dev', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello dev\n');
 });
 
